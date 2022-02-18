e27ee4f7a2f1358bc42b6954f0cb131a2d4029b3
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2022-02-18 22:41:08 +0900

    Create 20220218_bkjn_2741.cpp

---
+++ BKJN/20220218_bkjn_2741.cpp
@@ -0,0 +1,26 @@
+1
+#include<stdio.h>
+2
+​
+3
+int main() {
+4
+​
+5
+    int n;
+6
+​
+7
+    scanf("%d", &n);
+8
+​
+9
+    for (int i = 1; i <= n; i++) {
+10
+        printf("%d\n", i);
+11
+    }
+12
+    return 0;
+13
+}
