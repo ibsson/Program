8b18ef849fc755320dff296dea0f6399ae3fa1f0
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2023-07-10 18:35:56 +0900

    Update 20230708_bkjn_1193.cpp

--- BKJN/20230708_bkjn_1193.cpp
+++ BKJN/20230708_bkjn_1193.cpp
@@ -1,43 +1,29 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int x, key = 0, i = 2, j = 1, k, cnt = 2;
-    
-    scanf("%d", &x);
-    
-    if(x == 1)
-    {
-        printf("1/1");
-        return 0;
-    }
-    
-    while(1)
-    {
-        if(i <= x && i + j >= x)
-        {
-            if(j % 2 == 1) key = 0;
-            else key = 1;
-            break;
-        }
-        i = i + j + 1;
-        j++;
-    }
-    
-    i = 2;
-    while(1)
-    {
-        j = 1;
-        for(k = i; k > 0; k--)
-        {
-            if(x == cnt)
-            {
-                printf("%d/%d", (key == 1) ? k : j, (key == 1) ? j : k);
-                return 0;
-            }
-            j++; cnt++;
-        }
-        i++;
-    }
-    return 0;
+int main() {
+	
+	int X = 0;
+	int k = 1;
+	
+	scanf("%d", &X);
+	
+	while(1){
+		if((k-1)*(k)/2 < X && (k)*(k+1)/2 >= X){
+			break;
+		}
+		k++;
+	}
+	
+	if(k % 2 == 0){
+		int a = k*(k+1)/2;
+		printf("%d/", k - (a - X));
+		printf("%d", a - X + 1);
+	}
+	else{
+		int a = k*(k+1)/2;
+		printf("%d/", a - X + 1);
+		printf("%d", k - (a - X));
+	}
+	
+	return 0;
 }
