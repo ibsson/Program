6c8f42ac7cacdafc5642c2ad457e7fab4d9bbd35
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2024-01-09 01:00:07 +0900

    Create 20240109_bkjn_1748.cpp

---
+++ BKJN/20240109_bkjn_1748.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void){
+	
+	int N = 0;
+	int cnt = 1;
+	int result = 0;
+	
+	scanf("%d", &N);
+	
+	while(cnt <= N){
+		
+		if(cnt < 10) result += 1;
+		else if(cnt < 100) result += 2;
+		else if(cnt < 1000) result += 3;
+		else if(cnt < 10000) result += 4;
+		else if(cnt < 100000) result += 5;
+		else if(cnt < 1000000) result += 6;
+		else if(cnt < 10000000) result += 7;
+		else if(cnt < 100000000) result += 8;
+		else result += 9;
+		
+		cnt++;
+	}
+	
+	printf("%d\n", result);
+	
+	return 0;
+}
