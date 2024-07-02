972f9c2d7dc66713f7168f7f0460c1bc0f9e2b16
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2024-07-02 23:51:38 +0900

    Update 20240701_bkjn_2606(바이러스).cpp

--- BKJN/20240701_bkjn_2606(바이러스).cpp
+++ BKJN/20240701_bkjn_2606(바이러스).cpp
@@ -1,4 +1,233 @@
-//인접리스트
+//인접리스트 BFS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Vertex {
+	int Data;
+	int Visited;
+
+	struct Vertex* Next;
+	struct Edge* AdjacencyList;
+}Vertex;
+
+typedef struct Edge {
+	struct Edge* Next;
+	Vertex* From;
+	Vertex* Target;
+}Edge;
+
+typedef struct Graph {
+	Vertex* Vertices;
+}Graph;
+
+typedef struct Node {
+	Vertex* V;
+	struct Node* Next;
+}Node;
+
+typedef struct LinkedQueue {
+	Node* Front;
+	Node* Rear;
+}LinkedQueue;
+
+Graph* Create_Graph() {
+
+	Graph* G = (Graph*)malloc(sizeof(Graph));
+
+	G->Vertices = NULL;
+
+	return G;
+}
+
+Vertex* Create_Vertex(int data) {
+
+	Vertex* V = (Vertex*)malloc(sizeof(Vertex));
+
+	V->Data = data;
+	V->Visited = 0;
+	V->Next = NULL;
+	V->AdjacencyList = NULL;
+
+	return V;
+}
+
+void Add_Vertex(Graph* Computer, Vertex* V) {
+
+	if (Computer->Vertices == NULL) {
+		Computer->Vertices = V;
+	}
+	else {
+		Vertex* tmp = Computer->Vertices;
+
+		while (tmp->Next != NULL) {
+			tmp = tmp->Next;
+		}
+
+		tmp->Next = V;
+	}
+}
+
+Vertex* Find_Vertex(Graph* Computer, int data) {
+
+	Vertex* V = Computer->Vertices;
+
+	while (V != NULL) {
+		if (V->Data == data) break;
+
+		V = V->Next;
+	}
+
+	return V;
+}
+
+Edge* Create_Edge(Vertex* from, Vertex* target) {
+
+	Edge* E = (Edge*)malloc(sizeof(Edge));
+
+	E->From = from;
+	E->Target = target;
+	E->Next = NULL;
+
+	return E;
+}
+
+void Add_Edge(Vertex* V1, Edge* E) {
+
+	if (V1->AdjacencyList == NULL) {
+		V1->AdjacencyList = E;
+	}
+	else {
+		Edge* tmp = V1->AdjacencyList;
+		
+		while (tmp->Next != NULL) {
+			tmp = tmp->Next;
+		}
+
+		tmp->Next = E;
+	}
+}
+
+LinkedQueue* Create_Queue() {
+
+	LinkedQueue* Queue = (LinkedQueue*)malloc(sizeof(LinkedQueue));
+
+	Queue->Front = NULL;
+	Queue->Rear = NULL;
+
+	return Queue;
+}
+
+Node* Create_Node(Vertex* V1) {
+
+	Node* NewNode = (Node*)malloc(sizeof(Node));
+
+	NewNode->V = V1;
+	NewNode->Next = NULL;
+
+	return NewNode;
+}
+
+void Enqueue(LinkedQueue* Queue, Node* V) {
+
+	if (Queue->Front == NULL) {
+		Queue->Front = V;
+		Queue->Rear = V;
+	}
+	else {
+		Queue->Rear->Next = V;
+		Queue->Rear = V;
+	}
+}
+
+Node* Dequeue(LinkedQueue* Queue) {
+
+	Node* tmp = Queue->Front;
+
+	Queue->Front = Queue->Front->Next;
+
+	return tmp;
+}
+
+int Is_Empty(LinkedQueue* Queue) {
+
+	if (Queue->Front == NULL) return 0;
+	else return 1;
+}
+
+int BFS(Vertex* V, LinkedQueue* Queue) {
+
+	int cnt = 0;
+	Edge* E = NULL;
+
+	V->Visited = 1;
+
+	Enqueue(Queue, Create_Node(V));
+
+	while (Is_Empty(Queue) != 0) {
+		Node* Popped = Dequeue(Queue);
+
+		V = Popped->V;
+		E = V->AdjacencyList;
+
+		while (E != NULL) {
+			V = E->Target;
+
+			if (V->Visited != 1) {
+				V->Visited = 1;
+				cnt++;
+				Enqueue(Queue, Create_Node(V));
+			}
+
+			E = E->Next;
+		}
+	}
+
+	return cnt;
+}
+
+int main(void) {
+
+	int T = 0, N = 0;
+
+	scanf("%d", &T);
+
+	Graph* Computer = Create_Graph();
+
+	for (int i = 1; i <= T; i++) {
+		Add_Vertex(Computer, Create_Vertex(i)); //그래프에 정점 추가
+	}
+
+	scanf("%d", &N);
+
+	int From = 0, Target = 0;
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d %d", &From, &Target);
+
+		Vertex* V1 = Find_Vertex(Computer, From);
+		Vertex* V2 = Find_Vertex(Computer, Target);
+
+		Add_Edge(V1, Create_Edge(V1, V2)); //그래프의 정점에 인접한 정점들 추가
+		Add_Edge(V2, Create_Edge(V2, V1));
+	}
+
+	LinkedQueue* Queue = Create_Queue();
+
+	printf("%d\n", BFS(Computer->Vertices, Queue));
+
+	return 0;
+}
+
+/*
+BFS를 처음 구현해보았다.
+처음에는 이해하는데 조금 오래 걸렸지만 한번 원리를 이해하고 나니 구현하기는 쉬웠다.
+이 코드를 만들면서 DFS와 BFS의 차이점을 명확히 알게 되었다.
+앞으로는 문제를 보고 DFS와 BFS중 뭐가 더 좋은 풀이방식인지 구별하는것을 잘 하도록 노력해야겠다.
+그리고 DFS와 BFS를 모두 까먹지 않게 계속해서 만들어보면서 정말 내것으로 만들수 있도록 노력해야겠다.
+*/
+
+//인접리스트 DFS
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -169,7 +398,7 @@ int main(void) {
 DFS 뿐만 아니라 BFS로도 만들어봐야겠다.
 */
 
-//인접행렬
+//인접행렬 DFS
 
 #include <stdio.h>
 #include <stdlib.h>
