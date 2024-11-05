dd05ad966c944f134f3956afa37d6fea306f7d23
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2024-11-06 00:59:46 +0900

    Create 20241106_bkjn_1935(후위 표기식2).cpp

---
+++ BKJN/20241106_bkjn_1935(후위 표기식2).cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node {
+public:
+    double Data;
+};
+
+class ArrStack {
+public:
+    int top;
+    Node* Nodes;
+
+    void Create_Node(int Size) {
+        top = -1;
+        Nodes = new Node[Size];
+    }
+
+    void Push(double data) {
+        Nodes[++top].Data = data;
+    }
+
+    double Pop() {
+        return Nodes[top--].Data;
+    }
+};
+
+int main(void) {
+    int N = 0;
+    cin >> N;
+
+    string str;
+    cin >> str;
+
+    double* num = new double[N];
+    for (int i = 0; i < N; i++) {
+        cin >> num[i];
+    }
+
+    ArrStack* Stack = new ArrStack;
+    Stack->Create_Node(N);
+
+    double sum = 0, tmp_1 = 0, tmp_2 = 0;
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+            Stack->Push(num[str[i] - 'A']);
+        }
+        else {
+            tmp_1 = Stack->Pop();
+            tmp_2 = Stack->Pop();
+
+            if (str[i] == '+') {
+                sum = tmp_2 + tmp_1;
+            }
+            else if (str[i] == '-') {
+                sum = tmp_2 - tmp_1;
+            }
+            else if (str[i] == '*') {
+                sum = tmp_2 * tmp_1;
+            }
+            else if (str[i] == '/') {
+                sum = tmp_2 / tmp_1;
+            }
+            Stack->Push(sum);
+        }
+    }
+
+    cout << fixed;
+    cout.precision(2);
+    cout << Stack->Pop() << endl;
+
+    delete[] num;
+    delete[] Stack->Nodes;
+    delete Stack;
+
+    return 0;
+}
+
+/*
+c++로 스택을 처음으로 구현해서 만들어 보았다.
+c언어와 유사하지만 약간 차이점이 있어서 헷갈렸다. 계속 하다보면 적응 될 것이라고 생각한다.
+전에 후위표기식을 계산하는 문제를 구현해보아서 구현은 쉬웠다. 하지만 c++로 처음 스택을 구현해서 조금 시간이 걸렸다.
+class를 사용해서 스택을 만들었다.
+c++을 계속 사용해서 더욱 익숙해져야겠다.
+*/
