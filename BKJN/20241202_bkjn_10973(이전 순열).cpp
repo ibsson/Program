21947aa1c53a8d566bbc10770a880216bb908ce5
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2024-12-02 23:36:02 +0900

    Create 20241202_bkjn_10973(이전 순열).cpp

---
+++ BKJN/20241202_bkjn_10973(이전 순열).cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int chk;
+int arr[10001];
+int result[10001];
+
+bool Compare(int a, int b) {
+	return a > b;
+}
+
+int main(void) {
+
+	int N = 0;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	int tmp = 0;
+
+	for (int i = (N - 1); i > 0; i--) {
+		if (arr[i] < arr[i - 1]) {
+			chk = 1;
+			for (int j = (N - 1); j >= (i - 1); j--) {
+				if (arr[j] < arr[i - 1]) {
+					tmp = arr[j];
+					arr[j] = arr[i - 1];
+					arr[i - 1] = tmp;
+					sort(arr + i, arr + N, Compare);
+					break;
+				}
+			}
+		}
+		if (chk) break;
+	}
+
+	if (!chk) {
+		cout << "-1" << endl;
+	}
+	else {
+		for (int i = 0; i < N; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
