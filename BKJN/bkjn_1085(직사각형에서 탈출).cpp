5a2849738ec7cacc9ff09aab839b038a7a7459ce
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2024-12-21 17:00:08 +0900

    Create bkjn_1085(직사각형에서 탈출).cpp

---
+++ BKJN/bkjn_1085(직사각형에서 탈출).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+    int x = 0, y = 0, w = 0, h = 0;
+    int tmp1 = 0, tmp2 = 0;
+  
+    cin >> x >> y >> w >> h;
+    tmp1 = min(x,y);
+    tmp2 = min(w-x,h-y);
+    cout << min(tmp1,tmp2);
+}
