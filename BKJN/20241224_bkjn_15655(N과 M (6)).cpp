488f2d15d2b608d3fee09516c5cca9d64933deb0
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2024-12-24 15:42:04 +0900

    Create 20241224_bkjn_15655(N과 M (6)).cpp

---
+++ BKJN/20241224_bkjn_15655(N과 M (6)).cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int num[10];
+int arr[10];
+int Visited[10];
+
+void Back_Tracking(int idx, int cnt) {
+	arr[cnt] = num[idx];
+
+	if (cnt == M) {
+		for (int i = 1; i <= M; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		if (i > idx) {
+			if (!Visited[i]) {
+				Visited[i] = 1;
+				Back_Tracking(i, cnt + 1);
+				Visited[i] = 0;
+			}
+		}
+	}
+}
+
+int main(void) {
+
+	cin >> N >> M;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> num[i];
+	}
+
+	sort(num + 1, num + (N + 1));
+
+	Back_Tracking(0, 0);
+
+	return 0;
+}
