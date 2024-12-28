0e2aca12a4e7542378fc1c7258046e43b7c4ce04
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2024-12-28 17:42:14 +0900

    Create 20241228_bkjn_24480(알고리즘 수업 - 깊이 우선 탐색 2).cpp

---
+++ BKJN/20241228_bkjn_24480(알고리즘 수업 - 깊이 우선 탐색 2).cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define MAX 100002
+
+int N, M, R;
+int cnt = 1;
+bool Visited[MAX];
+int arr[MAX];
+
+vector <int> v[MAX];
+
+void DFS(int x) {
+	Visited[x] = true;
+	arr[x] = cnt;
+	cnt++;
+
+	for (int i = 0; i < v[x].size(); i++) {
+		int next = v[x][i];
+
+		if (!Visited[next]) {
+			Visited[next] = true;
+			DFS(next);
+		}
+	}
+}
+
+bool Compare(int a, int b) {
+	return a > b;
+}
+
+int main(void) {
+
+	cin >> N >> M >> R;
+
+	int from = 0, to = 0;
+
+	for (int i = 0; i < M; i++) {
+		cin >> from >> to;
+
+		v[from].push_back(to);
+		v[to].push_back(from);
+	}
+
+	for (int i = 1; i <= N; i++) {
+		sort(v[i].begin(), v[i].end(), Compare);
+	}
+
+	DFS(R);
+
+	for (int i = 1; i <= N; i++) {
+		if (!Visited[i]) arr[i] = 0;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		cout << arr[i] << "\n";
+	}
+
+	return 0;
+}
