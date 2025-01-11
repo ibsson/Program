21386060b773b211041cdc7db004dd68e1f38410
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-01-11 22:00:51 +0900

    Update 20250111_bkjn_16198(에너지 모으기).cpp

--- BKJN/20250111_bkjn_16198(에너지 모으기).cpp
+++ BKJN/20250111_bkjn_16198(에너지 모으기).cpp
@@ -36,3 +36,9 @@ int main(void) {
 
 	return 0;
 }
+
+/*
+v.begin() + 0 이 첫번째 index이고, v.begin() + 1이 두번째 index이다.
+만약 1 2 3에서 v.erase(v.begin() + 1)을 하면 1 3이 되고,
+1 2 3에서 v.insert(v.begin() + 2, 5)를 하면 1 2 4 3이 된다.
+*/
