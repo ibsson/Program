c66a10ce2ce2b0c30e3b982afca9a79e3fb65f56
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-01-12 18:22:55 +0900

    Create 20250112_bkjn_1987(알파벳).cpp

---
+++ BKJN/20250112_bkjn_1987(알파벳).cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 21
+
+int R, C, max_result;
+char board[MAX][MAX];
+bool visited[27];
+int dir[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+
+void dfs(int row, int col, int cnt) {
+	for (int i = 0; i < 4; i++) {
+		int n_row = row + dir[i][0];
+		int n_col = col + dir[i][1];
+
+		if (n_row >= 0 && n_row < R && n_col >= 0 && n_col < C) {
+			if (!visited[board[n_row][n_col] - 'A']) {
+				visited[board[n_row][n_col] - 'A'] = true;
+				dfs(n_row, n_col, cnt + 1);
+				visited[board[n_row][n_col] - 'A'] = false;
+			}
+		}
+
+		if (i == 3) {
+			max_result = max(max_result, cnt);
+		}
+	}
+}
+
+int main(void) {
+
+	cin >> R >> C;
+
+	for (int i = 0; i < R; i++) {
+		cin >> board[i];
+	}
+
+	visited[board[0][0] - 'A'] = true;
+	dfs(0, 0, 1);
+
+	cout << max_result << "\n";
+
+	return 0;
+}
