c4c167ae02a9b6a36362b25a6bb94c7f37e1a44a
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-01-21 18:35:39 +0900

    Create 20250121_bkjn_1931(회의실 배정).cpp

---
+++ BKJN/그리디 알고리즘/20250121_bkjn_1931(회의실 배정).cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+vector <pair<int, int>> v;
+
+bool Compare(pair<int, int> a, pair<int, int> b) {
+	if (a.second == b.second) return a.first < b.first;
+	else return a.second < b.second;
+}
+
+int main(void) {
+
+	cin >> N;
+
+	int from = 0, to = 0;
+
+	for (int i = 0; i < N; i++) {
+		cin >> from >> to;
+
+		v.push_back({ from, to });
+	}
+
+	sort(v.begin(), v.end(), Compare);
+
+	int a = v[0].first, b = v[0].second;
+	int cnt = 1;
+
+	for (int i = 1; i < v.size(); i++) {
+		if (v[i].first >= b) {
+			a = v[i].first, b = v[i].second;
+			cnt++;
+		}
+	}
+
+	cout << cnt << "\n";
+
+	return 0;
+}
