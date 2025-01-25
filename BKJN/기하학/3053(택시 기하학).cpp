2b384bf7a4fbc8088630f7aae540266cf574fe66
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-01-25 14:17:49 +0900

    Create 3053(택시 기하학).cpp

---
+++ BKJN/기하학/3053(택시 기하학).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#define PI 3.1415926535897932
+
+int main(void) {
+	double r = 0.0;
+
+	cin >> r;
+
+	cout << fixed;
+	cout.precision(6);
+
+	cout << r * r * PI << "\n";
+	cout << 2 * r * r << "\n";
+}
