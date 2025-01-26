38a0a1082680ca3324f0a26ef3a2f3ff74964ed9
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-01-26 22:19:52 +0900

    Create 20250126_bkjn_17404(RGB거리 2).cpp

---
+++ BKJN/DP/20250126_bkjn_17404(RGB거리 2).cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+using namespace std;
+
+#define MAX 1000000
+int N;
+int color[1001][3];
+int dp[1001][3];
+int ans = MAX;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+		cin >> color[i][0] >> color[i][1] >> color[i][2];
+
+	for (int RGB = 0; RGB <= 2; RGB++) {
+		for (int i = 0; i <= 2; i++) {
+			if (i == RGB) dp[0][i] = color[0][i];
+			else dp[0][i] = MAX;
+		}
+
+		for (int i = 1; i < N; i++) {
+			dp[i][0] = color[i][0] + min(dp[i - 1][1], dp[i - 1][2]);
+			dp[i][1] = color[i][1] + min(dp[i - 1][0], dp[i - 1][2]);
+			dp[i][2] = color[i][2] + min(dp[i - 1][0], dp[i - 1][1]);
+		}
+
+		for (int i = 0; i <= 2; i++) {
+			if (i == RGB) continue;
+			else ans = min(ans, dp[N - 1][i]);
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
