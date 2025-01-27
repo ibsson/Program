33df2b91c0ed411aa2d4afc1f0c49e17b176e100
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-01-28 01:17:35 +0900

    Create 20250128_bkjn_1744(수 묶기).cpp

---
+++ BKJN/그리디 알고리즘/20250128_bkjn_1744(수 묶기).cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, cnt_p, cnt_m;
+int arr[51];
+
+int main(void) {
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+
+		if (arr[i] > 0) cnt_p++;
+		if (arr[i] <= 0) cnt_m++;
+	}
+
+	sort(arr, arr + N);
+
+	vector <int> p;
+	vector <int> m;
+
+	for (int i = 0; i < cnt_m; i++) {
+		m.push_back(arr[i]);
+	}
+
+	for (int i = (N - 1); i > (N - 1) - cnt_p; i--) {
+		p.push_back(arr[i]);
+	}
+
+	int result = 0;
+
+	for (int i = 0; i < cnt_m; i++) {
+		if (i == cnt_m - 1) {
+			result += m[i];
+			break;
+		}
+
+		result += m[i] * m[i + 1];
+		i++;
+	}
+
+	for (int i = 0; i < cnt_p; i++) {
+		if (i == cnt_p - 1) {
+			result += p[i];
+			break;
+		}
+
+		if (p[i] == 1 || p[i + 1] == 1) {
+			result += p[i];
+		}
+		else {
+			result += p[i] * p[i + 1];
+			i++;
+		}
+	}
+
+	cout << result << "\n";
+
+	return 0;
+}
