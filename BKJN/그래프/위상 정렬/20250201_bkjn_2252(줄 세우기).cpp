b87e35fbf3067b95155e7722cd941d9f89236ac6
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-02-01 14:29:27 +0900

    Create 20250201_bkjn_2252(줄 세우기).cpp

---
+++ BKJN/그래프/위상 정렬/20250201_bkjn_2252(줄 세우기).cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+#define MAX 32001
+
+int N, M;
+int indegree[MAX];
+vector <int> graph[MAX];
+
+void topologySort() {
+	queue <int> q;
+
+	for (int i = 1; i <= N; i++) {
+		if (!indegree[i]) q.push(i);
+	}
+
+	for (int i = 0; i < N; i++) {
+		int from = q.front();
+		cout << from << " ";
+		q.pop();
+
+		for (int j = 0; j < graph[from].size(); j++) {
+			int to = graph[from][j];
+			indegree[to]--;
+			if (!indegree[to]) q.push(to);
+		}
+	}
+}
+
+int main() {
+
+	cin >> N >> M;
+
+	int A = 0, B = 0;
+
+	for (int i = 0; i < M; i++) {
+		cin >> A >> B;
+
+		graph[A].push_back(B);
+		indegree[B]++;
+	}
+
+	topologySort();
+
+	return 0;
+}
