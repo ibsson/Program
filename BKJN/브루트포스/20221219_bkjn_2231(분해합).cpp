208a03a49238c23df2cd1a39ef7e7072f0194802
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-02-10 01:12:40 +0900

    Update and rename 20221219_bkjn_2231_1(분해합).cpp to 20221219_bkjn_2231(분해합).cpp

--- BKJN/브루트포스/20221219_bkjn_2231(분해합).cpp
+++ BKJN/브루트포스/20221219_bkjn_2231(분해합).cpp
@@ -1,5 +1,41 @@
 #include <stdio.h>
 
+int cal(int n) {
+
+	int room = 0;
+
+	for (int i = 1; i <= n; i++) {
+		int tmp = i;
+		int sum = i;
+		while (tmp != 0) {
+			sum += (tmp % 10);
+			tmp /= 10;
+		}
+		if (sum == n) {
+			room = i;
+			break;
+		}
+	}
+
+	return room;
+}
+
+int main() {
+
+	int num = 0;
+
+	scanf_s("%d", &num);
+
+	int result = cal(num);
+
+	printf("%d\n", result);
+
+	return 0;
+}
+
+/*
+#include <stdio.h>
+
 int main() {
 
 	int num = 0;
@@ -58,3 +94,4 @@ int main() {
 
 	return 0;
 }
+*/
