e97ca9226924a07e51e9a9f25abced2cdbed6cbf
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-02-20 00:00:33 +0900

    Create 20250220_bkjn_9184(신나는 함수 실행).cpp

---
+++ BKJN/DP/20250220_bkjn_9184(신나는 함수 실행).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int dp[21][21][21];
+
+int solve(int a, int b, int c) {
+	if (a <= 0 || b <= 0 || c <= 0) return 1;
+
+	if (a > 20 || b > 20 || c > 20) return solve(20, 20, 20);
+
+	if (dp[a][b][c] != 0) return dp[a][b][c];
+
+	if (a < b && b < c) {
+		dp[a][b][c] = solve(a, b, c - 1) + solve(a, b - 1, c - 1) - solve(a, b - 1, c);
+		return dp[a][b][c];
+	}
+
+	dp[a][b][c] = solve(a - 1, b, c) + solve(a - 1, b - 1, c) + solve(a - 1, b, c - 1) - solve(a - 1, b - 1, c - 1);
+	return dp[a][b][c];
+}
+
+int main() {
+
+	int a = 0, b = 0, c = 0;
+
+	while (1) {
+		cin >> a >> b >> c;
+
+		if (a == -1 && b == -1 && c == -1) break;
+
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << solve(a, b, c) << "\n";
+	}
+
+	return 0;
+}
