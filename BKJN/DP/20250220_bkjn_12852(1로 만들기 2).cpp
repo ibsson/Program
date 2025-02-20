5b1d84ceee81e2f92e50bd045729bdc4f7400e80
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-02-20 23:37:57 +0900

    Update 20250220_bkjn_12852(1로 만들기 2).cpp

--- BKJN/DP/20250220_bkjn_12852(1로 만들기 2).cpp
+++ BKJN/DP/20250220_bkjn_12852(1로 만들기 2).cpp
@@ -1,4 +1,53 @@
 #include <iostream>
+using namespace std;
+
+#define MAX 1000001
+
+int X;
+int dp[MAX];
+
+int main() {
+
+	cin >> X;
+
+	dp[1] = 0;
+
+	for (int i = 2; i <= X; i++) {
+		int tmp = dp[i - 1] + 1;
+
+		if (i % 2 == 0) {
+			if (tmp > dp[i / 2] + 1) {
+				tmp = dp[i / 2] + 1;
+			}
+		}
+
+		if (i % 3 == 0) {
+			if (tmp > dp[i / 3] + 1) {
+				tmp = dp[i / 3] + 1;
+			}
+		}
+
+		dp[i] = tmp;
+	}
+
+	cout << dp[X] << "\n";
+
+	int cur = X;
+	while (1) {
+		cout << cur << " ";
+
+		if (cur == 1) break;
+
+		if (dp[cur] == dp[cur - 1] + 1) cur -= 1;
+		else if (cur % 2 == 0 && dp[cur] == dp[cur / 2] + 1) cur /= 2;
+		else if (cur % 3 == 0 && dp[cur] == dp[cur / 3] + 1) cur /= 3;
+	}
+
+	return 0;
+}
+
+/*
+#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -48,3 +97,4 @@ int main() {
 
 	return 0;
 }
+*/
