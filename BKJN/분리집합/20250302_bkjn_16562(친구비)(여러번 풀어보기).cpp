e3b777493881f9d5b02fa58d27f87ba8d74b2894
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-03-02 01:41:01 +0900

    Create 20250302_bkjn_16562(친구비)(여러번 풀어보기).cpp

---
+++ BKJN/분리집합/20250302_bkjn_16562(친구비)(여러번 풀어보기).cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+#define MAX 10001
+
+int N, M, k, ans;
+int pay[MAX];
+int parent[MAX];
+
+int findParent(int x) {
+	if (parent[x] != x) {
+		return parent[x] = findParent(parent[x]);
+	}
+	return x;
+}
+
+void merge(int a, int b) {
+	int x = findParent(a);
+	int y = findParent(b);
+
+	if (x != y) {
+		if (pay[x] < pay[y]) {
+			parent[y] = x;
+		}
+		else {
+			parent[x] = y;
+		}
+	}
+}
+
+int main() {
+
+	cin >> N >> M >> k;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> pay[i];
+		parent[i] = i;
+	}
+
+
+	int from = 0, to = 0;
+	for (int i = 0; i < M; i++) {
+		cin >> from >> to;
+
+		merge(from, to);
+	}
+
+	for (int i = 1; i <= N; i++) {
+		int root = findParent(i);
+		if (root != findParent(0)) {
+			ans += pay[root];
+			merge(0, i);
+		}
+	}
+	
+	if (k >= ans) {
+		cout << ans << "\n";
+	}
+	else {
+		cout << "Oh no\n";
+	}
+
+	return 0;
+}
