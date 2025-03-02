676bf256cb6d98e796ecffb7227cd9961ddfdf3c
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-03-02 10:44:35 +0900

    Update 20250302_bkjn_16562(친구비)(여러번 풀어보기).cpp

--- BKJN/분리집합/20250302_bkjn_16562(친구비)(여러번 풀어보기).cpp
+++ BKJN/분리집합/20250302_bkjn_16562(친구비)(여러번 풀어보기).cpp
@@ -1,3 +1,70 @@
+//복습
+
+#include <iostream>
+using namespace std;
+
+#define MAX 10001
+
+int N, M, k, ans;
+int pay[MAX];
+int parent[MAX];
+
+int findParent(int x) {
+	if (parent[x] != x) return parent[x] = findParent(parent[x]);
+	return x;
+}
+
+void merge(int x, int y) {
+	int a = findParent(x);
+	int b = findParent(y);
+
+	if (a != b) {
+		if (pay[a] > pay[b]) {
+			parent[a] = b;
+		}
+		else {
+			parent[b] = a;
+		}
+	}
+}
+
+int main() {
+
+	cin >> N >> M >> k;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> pay[i];
+		parent[i] = i;
+	}
+
+	int from = 0, to = 0;
+	for (int i = 0; i < M; i++) {
+		cin >> from >> to;
+
+		merge(from, to);
+	}
+
+	for (int i = 1; i <= N; i++) {
+		int x = findParent(i);
+		int y = findParent(0);
+
+		if (x != y) {
+			ans += pay[x];
+			merge(0, i);
+		}
+	}
+
+	if (k >= ans) {
+		cout << ans << "\n";
+	}
+	else {
+		cout << "Oh no\n";
+	}
+
+	return 0;
+}
+
+/*
 #include <iostream>
 #include <vector>
 #include <climits>
@@ -64,3 +131,4 @@ int main() {
 
 	return 0;
 }
+*/
