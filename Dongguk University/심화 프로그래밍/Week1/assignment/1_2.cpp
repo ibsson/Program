1d8fa7340dd15186633d0704f5ba271a05a02686
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-03-05 11:13:06 +0900

    Create 1_2.cpp

---
+++ Dongguk University/심화 프로그래밍/Week1/assignment/1_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //입출력문을 위한 헤더파일
+
+int main() {
+	int num1 = 1, num2 = 10, ans = 55;
+	//변수 num1, num2, ans 선언
+
+	std::cout << num1 << "에서 " << num2 << "까지 더한 결과는 " << ans << "입니다.";
+	//std::cout : C++의 표준 출력, 변수 num1, num2, ans 에 저장된 1, 10, 55 출력
+
+	return 0;
+}
