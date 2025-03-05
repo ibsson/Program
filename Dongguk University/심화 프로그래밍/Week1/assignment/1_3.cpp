50e3c0581343d025eea8822256a494dd8b8dac3a
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-03-05 11:13:23 +0900

    Create 1_3.cpp

---
+++ Dongguk University/심화 프로그래밍/Week1/assignment/1_3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+	std::cout << "멀티미디어공학과" << std::endl;
+	//std::cout : C++의 표준 출력, std::endl : C++의 줄 바꾸기
+	std::cout << "22세" << std::endl;
+	//std::cout : C++의 표준 출력, std::endl : C++의 줄 바꾸기
+	std::cout << "인공지능 개발자" << std::endl;
+	//std::cout : C++의 표준 출력, std::endl : C++의 줄 바꾸기
+
+	return 0;
+}
