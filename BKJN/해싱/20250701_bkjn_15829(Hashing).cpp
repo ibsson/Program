6f7f655f010fbf36e87d8fdbcc1cf00c05736661
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-07-01 17:24:48 +0900

    Create 20250701_bkjn_15829(Hashing).cpp

---
+++ BKJN/해싱/20250701_bkjn_15829(Hashing).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define NUM 1234567891
+
+int main() {
+
+	long long int L;
+	cin >> L;
+
+	string str;
+	cin >> str;
+
+	long long int res = 0;
+
+	long long int r = 1;
+	for (int i = 0; i < L; i++) {
+		int a = (str[i] - 'a' + 1) % NUM;
+		int tmp = (a * r) % NUM;
+
+		res = (res + tmp) % NUM;
+
+		r = (r * (31 % NUM)) % NUM;
+	}
+
+	res %= NUM;
+
+	cout << res << "\n";
+
+	return 0;
+}
