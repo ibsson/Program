22c044f8eb66d8aadf46fddf81f92dd00980111d
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-07-05 23:12:17 +0900

    Create 20250705_bkjn_7891(Can you add this?).cpp

---
+++ BKJN/수학/20250705_bkjn_7891(Can you add this?).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int t;
+int main(){
+    cin >> t;
+    while(t--){
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+    
+    return 0;
+}
