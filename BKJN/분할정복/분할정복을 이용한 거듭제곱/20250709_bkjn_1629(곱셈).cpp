c4fcc4b559468c04341c9ea715b5c5af98b4e13e
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-07-09 14:56:17 +0900

    Create 20250709_bkjn_1629(곱셈).cpp

---
+++ BKJN/분할정복/분할정복을 이용한 거듭제곱/20250709_bkjn_1629(곱셈).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int cal(long long int A, int B, int C) {
+	long long int res = 1;
+
+	while (B > 0) {
+		if (B % 2 == 1) {
+			res *= (A % C);
+			res %= C;
+		}
+		A *= (A % C);
+		A %= C;
+		B = B / 2;
+	}
+
+	return res;
+}
+
+int main() {
+
+	long long int A;
+	int B, C;
+	cin >> A >> B >> C;
+
+	cout << cal(A, B, C) << '\n';
+
+	return 0;
+}
