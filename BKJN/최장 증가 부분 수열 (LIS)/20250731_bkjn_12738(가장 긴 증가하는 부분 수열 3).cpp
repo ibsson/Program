8ffe085128ff3c982635974bfcad7d3ca8dcc32e
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-07-31 15:35:35 +0900

    Create 20250731_bkjn_12738(가장 긴 증가하는 부분 수열 3).cpp

---
+++ BKJN/최장 증가 부분 수열 (LIS)/20250731_bkjn_12738(가장 긴 증가하는 부분 수열 3).cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int getIdx(vector <int>& LIS, int val) {
+	int start = 0, end = LIS.size() - 1, mid = 0;
+	while (start <= end) {
+		mid = (start + end) / 2;
+
+		if (LIS[mid] < val) {
+			start = mid + 1;
+		}
+		else {
+			end = mid - 1;
+		}
+	}
+
+	return start;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	vector <int> v;
+	
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+
+	vector <int> LIS;
+
+	for (int i = 0; i < N; i++) {
+		int idx = getIdx(LIS, v[i]);
+
+		if (idx == LIS.size()) {
+			LIS.push_back(v[i]);
+		}
+		else {
+			LIS[idx] = v[i];
+		}
+	}
+
+	cout << LIS.size() << '\n';
+
+	return 0;
+}
