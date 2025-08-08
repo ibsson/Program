32830795e2ef4676f89018c100300805fbe21a77
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-08 16:42:34 +0900

    Create 20250808_bkjn_1707(이분 그래프).cpp

---
+++ BKJN/그래프/이분 그래프/20250808_bkjn_1707(이분 그래프).cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#define MAX 20001
+#define RED 1
+#define BLUE 2
+using namespace std;
+
+vector <int> graph[MAX];
+int visited[MAX];
+
+void DFS(int cur) {
+	if (!visited[cur]) visited[cur] = RED;
+
+	for (int i = 0; i < graph[cur].size(); i++) {
+		int next = graph[cur][i];
+
+		if (!visited[next]) {
+			if (visited[cur] == RED) visited[next] = BLUE;
+			else if (visited[cur] == BLUE) visited[next] = RED;
+
+			DFS(next);
+		}
+	}
+}
+
+bool isBinGraph(int V) {
+	for (int cur = 1; cur <= V; cur++) {
+		for (int i = 0; i < graph[cur].size(); i++) {
+			int next = graph[cur][i];
+
+			if (visited[cur] == visited[next]) return false;
+		}
+	}
+
+	return true;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int K;
+	cin >> K;
+
+	for (int t = 1; t <= K; t++) {
+		int V, E;
+		cin >> V >> E;
+
+		int u, v;
+		for (int i = 0; i < E; i++) {
+			cin >> u >> v;
+			graph[u].push_back(v);
+			graph[v].push_back(u);
+		}
+
+		for (int i = 1; i <= V; i++) {
+			if (!visited[i]) DFS(i);
+		}
+
+		if (isBinGraph(V)) cout << "YES\n";
+		else cout << "NO\n";
+
+		for (int i = 1; i <= V; i++) {
+			graph[i].clear();
+			visited[i] = false;
+		}
+	}
+
+	return 0;
+}
