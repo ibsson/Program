56d41a241018976775cfa5e70b4dca654aa955a9
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-19 14:30:13 +0900

    Create 20250819_bkjn_9093(단어 뒤집기).cpp

---
+++ BKJN/자료구조/스택/20250819_bkjn_9093(단어 뒤집기).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	cin.ignore();
+
+	for (int t = 1; t <= T; t++) {
+		string str;
+		stack <char> s;
+		getline(cin, str);
+
+		for (int i = 0; i <= str.length(); i++) {
+			if (str[i] == ' ' || i == str.length()) {
+				while (!s.empty()) {
+					cout << s.top();
+					s.pop();
+				}
+				cout << ' ';
+				continue;
+			}
+
+			s.push(str[i]);
+		}
+
+		cout << '\n';
+	}
+
+	return 0;
+}
