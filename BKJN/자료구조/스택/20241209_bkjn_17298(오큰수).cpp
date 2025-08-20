b0d6272ff81f11c33540bd6c5703308e3496e821
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-20 11:46:40 +0900

    Update 20241209_bkjn_17298(오큰수).cpp

--- BKJN/자료구조/스택/20241209_bkjn_17298(오큰수).cpp
+++ BKJN/자료구조/스택/20241209_bkjn_17298(오큰수).cpp
@@ -1,5 +1,53 @@
 #include <iostream>
 #include <stack>
+#include <vector>
+using namespace std;
+
+stack <int> s;
+stack <int> res;
+vector <int> v;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+	
+	s.push(v[N - 1]);
+	res.push(-1);
+	
+	for (int i = (N - 2); i >= 0; i--) {
+		while (!s.empty()) {
+			if (v[i] >= s.top()) s.pop();
+			else break;
+		}
+		
+		if (s.empty()) res.push(-1);
+		else res.push(s.top());
+
+		s.push(v[i]);
+	}
+
+	while (!res.empty()) {
+		cout << res.top() << ' ';
+		res.pop();
+	}
+
+	return 0;
+}
+
+/*
+
+#include <iostream>
+#include <stack>
 using namespace std;
 
 int a[1000001];
@@ -43,4 +91,5 @@ int main(void) {
 
 /*
 STL stack을 사용해서 쉽게 풀었다.
+*/
 */
