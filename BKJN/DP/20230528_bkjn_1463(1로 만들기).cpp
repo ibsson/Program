f75b767fb5c055d8c2b1d95d82d46bacc79690c1
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-20 14:48:57 +0900

    Update 20230528_bkjn_1463(1로 만들기).cpp

--- BKJN/DP/20230528_bkjn_1463(1로 만들기).cpp
+++ BKJN/DP/20230528_bkjn_1463(1로 만들기).cpp
@@ -1,3 +1,36 @@
+#include <iostream>
+#define MAX 10000000
+using namespace std;
+
+int dp[MAX];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 2; i <= N; i++) {
+		dp[i] = dp[i - 1] + 1;
+
+		if (i % 2 == 0) {
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+		}
+
+		if (i % 3 == 0) {
+			dp[i] = min(dp[i], dp[i / 3] + 1);
+		}
+	}
+
+	cout << dp[N] << '\n';
+
+	return 0;
+}
+
+/*
+
 #include <stdio.h>
  
 int min(int a,int b)
@@ -38,3 +71,4 @@ int main()
     scanf("%d",&a);
     printf("%d",dp(a));
 }
+*/
