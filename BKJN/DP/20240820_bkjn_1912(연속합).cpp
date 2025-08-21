c22509de5142376b14cd5d9e1b9237655a5f9b73
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-22 01:38:28 +0900

    Update 20240820_bkjn_1912(연속합).cpp

--- BKJN/DP/20240820_bkjn_1912(연속합).cpp
+++ BKJN/DP/20240820_bkjn_1912(연속합).cpp
@@ -1,3 +1,41 @@
+#include <iostream>
+#include <vector>
+#define MAX 100002
+using namespace std;
+
+vector <int> v;
+int dp[MAX];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+
+	int res = v[0];
+	dp[0] = v[0];
+	for (int i = 1; i < n; i++) {
+		dp[i] = v[i];
+
+		dp[i] = max(dp[i], dp[i - 1] + v[i]);
+
+		res = max(res, dp[i]);
+	}
+
+	cout << res << '\n';
+
+	return 0;
+}
+
+/*
 #include <stdio.h>
 
 int num[100002] = { 0 };
@@ -33,4 +71,5 @@ int main(void) {
 이 문제는 dp로 풀어냈다.
 바로 전의 연속합이 음수면 num[i]까지의 연속합의 최대는 자기자신이고, 
 아니면 num[i]까지의 연속합의 최대는 자기자신과 바로 전의 연속합을 더하는것이라는 규칙을 찾아내서 쉽게 풀었다.
+*/
 */
