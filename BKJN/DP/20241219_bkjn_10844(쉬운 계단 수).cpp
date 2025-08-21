bf6816c27df2c4d8c6ebca274d5a95c500f0eb9b
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-21 14:58:40 +0900

    Update 20241219_bkjn_10844(쉬운 계단 수).cpp

--- BKJN/DP/20241219_bkjn_10844(쉬운 계단 수).cpp
+++ BKJN/DP/20241219_bkjn_10844(쉬운 계단 수).cpp
@@ -1,4 +1,50 @@
 #include <iostream>
+#define MOD 1000000000
+using namespace std;
+
+int dp[101][10];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= 9; i++) {
+		dp[1][i] = 1;
+	}
+
+	for (int i = 2; i <= N; i++) {
+		for (int j = 0; j <= 9; j++) {
+			if (j == 0) {
+				dp[i][j] += (dp[i - 1][j + 1]);
+			}
+			else if(j == 9) {
+				dp[i][j] += dp[i - 1][j - 1];
+			}
+			else {
+				dp[i][j] += (dp[i - 1][j - 1] + dp[i - 1][j + 1]);
+			}
+
+			dp[i][j] %= MOD;
+		}
+	}
+
+	int res = 0;
+	for (int i = 0; i <= 9; i++) {
+		res += dp[N][i];
+		res %= MOD;
+	}
+
+	cout << res << '\n';
+
+	return 0;
+}
+
+/*
+#include <iostream>
 using namespace std;
 
 #define MOD 1000000000
@@ -42,3 +88,4 @@ int main(void) {
 
 	return 0;
 }
+*/
