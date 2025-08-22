70d6b8a1e4ca21f527bc03fd5f62632cd68688ce
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-22 14:58:07 +0900

    Update 20241114_bkjn_1699(제곱수의 합).cpp

--- BKJN/DP/20241114_bkjn_1699(제곱수의 합).cpp
+++ BKJN/DP/20241114_bkjn_1699(제곱수의 합).cpp
@@ -1,4 +1,35 @@
 #include <iostream>
+#include <cmath>
+#define MAX 100002
+#define INF 1e9
+using namespace std;
+
+int dp[MAX];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	dp[1] = 1, dp[2] = 2, dp[3] = 3;
+	for (int i = 4; i <= N; i++) {
+		dp[i] = INF;
+		for (int j = 1; pow(j, 2) <= i; j++) {
+			int tmp = pow(j, 2);
+			dp[i] = min(dp[i], dp[i - tmp] + 1);
+		}
+	}
+
+	cout << dp[N] << '\n';
+
+	return 0;
+}
+
+/*
+#include <iostream>
 using namespace std;
 
 int dp[100001] = { 0 };
@@ -25,3 +56,4 @@ int main(void) {
 
 	return 0;
 }
+*/
