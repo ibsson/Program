8380b1b336bb8876797b662f165cf73a25ebb3eb
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-24 15:49:46 +0900

    Update and rename 20241122_bkjn_15990(1, 2, 3 더하기 5).cpp (2차원 배열 활용 dp) to 20241122_bkjn_15990(1, 2, 3 더하기 5)(2차원 배열 활용 dp).cpp

--- BKJN/DP/20241122_bkjn_15990(1, 2, 3 더하기 5)(2차원 배열 활용 dp).cpp
+++ BKJN/DP/20241122_bkjn_15990(1, 2, 3 더하기 5)(2차원 배열 활용 dp).cpp
@@ -1,4 +1,40 @@
 #include <iostream>
+#define MAX 100002
+#define MOD 1000000009
+using namespace std;
+
+long long int dp[3][MAX];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int T;
+	cin >> T;
+
+	dp[0][1] = 1;
+	dp[1][2] = 1;
+	dp[0][3] = 1, dp[1][3] = 1, dp[2][3] = 1;
+
+	for (int i = 4; i < MAX; i++) {
+		dp[0][i] = (dp[1][i - 1] + dp[2][i - 1]) % MOD;
+		dp[1][i] = (dp[0][i - 2] + dp[2][i - 2]) % MOD;
+		dp[2][i] = (dp[0][i - 3] + dp[1][i - 3]) % MOD;
+	}
+
+	for (int t = 1; t <= T; t++) {
+		int n;
+		cin >> n;
+
+		cout << (dp[0][n] + dp[1][n] + dp[2][n]) % MOD << '\n';
+	}
+
+	return 0;
+}
+
+/*
+#include <iostream>
 using namespace std;
 
 const int mod = 1e9 + 9;
@@ -36,4 +72,5 @@ int main(void) {
 앞으로 dp문제를 풀때 시야를 넓힐 수 있을 것 같다.
 그리고 마지막에 나머지를 구할때 마지막 자리수 숫자를 기준으로 나눠진 개수들의 나머지를 더한것들을 똑같은 수로 나눠서 나머지를 구해주면
 마지막 자리수 숫자를 기준으로 나눠진 개수들의 합을 나눈 나머지와 같다는 것도 알 수 있었다.
+*/
 */
