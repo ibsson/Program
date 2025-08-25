7e6047669e32d39152a5c89beb617985d44bf779
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-25 10:52:08 +0900

    Update 20241127_bkjn_11057(오르막 수).cpp

--- BKJN/DP/20241127_bkjn_11057(오르막 수).cpp
+++ BKJN/DP/20241127_bkjn_11057(오르막 수).cpp
@@ -1,4 +1,43 @@
 #include <iostream>
+#define MAX 1002
+#define MOD 10007
+using namespace std;
+
+int dp[MAX][10];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i <= 9; i++) {
+		dp[1][i] = 1;
+	}
+
+	for (int i = 2; i <= N; i++) {
+		for (int j = 0; j <= 9; j++) {
+			for (int k = j; k >= 0; k--) {
+				dp[i][j] += (dp[i - 1][k]) % MOD;
+				dp[i][j] %= MOD;
+			}
+		}
+	}
+
+	int res = 0;
+	for (int i = 0; i <= 9; i++) {
+		res += (dp[N][i]) % MOD;
+		res %= MOD;
+	}
+	cout << res % MOD << '\n';
+
+	return 0;
+}
+
+/*
+#include <iostream>
 using namespace std;
 
 int dp[1001][10];
@@ -38,4 +77,5 @@ int main(void) {
 조금 시행착오가 있었지만 그래도 풀어냈다.
 문제를 풀때 정말 작은 차이로 문제가 발생할때가 많다.
 이런일을 방지하기 위해서 더 섬세하게 생각해서 문제를 풀어나가야겠다.
+*/
 */
