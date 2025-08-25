70325efb9ef657f5e744f2e4d6a8a85dc6998ae0
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-25 13:56:23 +0900

    Update 20241129_bkjn_9465(스티커).cpp

--- BKJN/DP/20241129_bkjn_9465(스티커).cpp
+++ BKJN/DP/20241129_bkjn_9465(스티커).cpp
@@ -1,4 +1,54 @@
 #include <iostream>
+#define MAX 100001
+using namespace std;
+
+int arr[2][MAX];
+int dp[2][MAX];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int T;
+	cin >> T;
+
+	for (int t = 1; t <= T; t++) {
+		int n;
+		cin >> n;
+
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < n; j++) {
+				cin >> arr[i][j];
+			}
+		}
+
+		dp[0][0] = arr[0][0], dp[1][0] = arr[1][0];
+		int _max = 0;
+		for (int i = 1; i < n; i++) {
+			if (i > 1) {
+				_max = max(dp[0][i - 2], dp[1][i - 2]);
+
+				if (_max > dp[1][i - 1]) dp[0][i] = _max + arr[0][i];
+				else dp[0][i] = dp[1][i - 1] + arr[0][i];
+
+				if (_max > dp[0][i - 1]) dp[1][i] = _max + arr[1][i];
+				else dp[1][i] = dp[0][i - 1] + arr[1][i];
+			}
+			else {
+				dp[0][i] = dp[1][i - 1] + arr[0][i];
+				dp[1][i] = dp[0][i - 1] + arr[1][i];
+			}
+		}
+
+		cout << max(dp[0][n - 1], dp[1][n - 1]) << '\n';
+	}
+
+	return 0;
+}
+
+/*
+#include <iostream>
 using namespace std;
 
 int stiker[2][100002];
@@ -43,3 +93,4 @@ int main(void) {
 
 	return 0;
 }
+*/
