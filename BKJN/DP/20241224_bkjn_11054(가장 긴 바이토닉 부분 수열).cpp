6052418d7c4c3f66cca3e73aea3cbbaaf109a541
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-27 10:41:17 +0900

    Update 20241224_bkjn_11054(가장 긴 바이토닉 부분 수열).cpp

--- BKJN/DP/20241224_bkjn_11054(가장 긴 바이토닉 부분 수열).cpp
+++ BKJN/DP/20241224_bkjn_11054(가장 긴 바이토닉 부분 수열).cpp
@@ -1,4 +1,58 @@
 #include <iostream>
+#include <vector>
+#define MAX 1002
+using namespace std;
+
+vector <int> A;
+int dp1[MAX];
+int dp2[MAX];
+int res[MAX];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		cin >> tmp;
+		A.push_back(tmp);
+	}
+
+	for (int i = 0; i < N; i++) {
+		dp1[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (A[i] > A[j]) {
+				dp1[i] = max(dp1[i], dp1[j] + 1);
+			}
+		}
+	}
+
+	for (int i = (N - 1); i >= 0; i--) {
+		dp2[i] = 1;
+		for (int j = (N - 1); j > i; j--) {
+			if (A[i] > A[j]) {
+				dp2[i] = max(dp2[i], dp2[j] + 1);
+			}
+		}
+	}
+	
+	int ans = 0;
+	for (int i = 0; i < N; i++) {
+		res[i] = dp1[i] + dp2[i] - 1;
+		ans = max(ans, res[i]);
+	}
+
+	cout << ans << '\n';
+
+	return 0;
+}
+
+/*
+#include <iostream>
 using namespace std;
 
 int num[1001];
@@ -49,4 +103,5 @@ int main(void) {
 최장 증가 부분 수열 (LIS)알고리즘을 활용했다.
 앞에서부터 비교한 최장 증가 길이와 뒤에서부터 비교한 최장 증가 길이를 더했을때 가장 큰 지점이 정답이다.
 조금 어려웠던 문제였다. 이런 생각의 도약을 해낼 수 있도록 더 노력해야겠다.
+*/
 */
