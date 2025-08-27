74c5f75f3052b0b042c621b5f1061a26e7e5005c
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-27 11:11:03 +0900

    Update 20241206_bkjn_13398(연속합 2).cpp

--- BKJN/DP/20241206_bkjn_13398(연속합 2).cpp
+++ BKJN/DP/20241206_bkjn_13398(연속합 2).cpp
@@ -1,4 +1,41 @@
 #include <iostream>
+#include <vector>
+#define MAX 100002
+using namespace std;
+
+vector <int> v;
+int dp[2][MAX];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+	
+	dp[0][0] = v[0], dp[1][0] = 0;
+	int res = v[0];
+	for (int i = 1; i < n; i++) {
+		dp[0][i] = max(dp[0][i - 1] + v[i], v[i]);
+		dp[1][i] = max(dp[0][i - 1], dp[1][i - 1] + v[i]);
+		int tmp = max(dp[0][i], dp[1][i]);
+		res = max(res, tmp);
+	}
+
+	cout << res << '\n';
+
+	return 0;
+}
+
+/*
+#include <iostream>
 using namespace std;
 
 int arr[100001];
@@ -51,4 +88,5 @@ int main(void) {
 처음으로 골드티어 문제를 풀어냈다.
 비록 온전히 내 힘으로 풀어낸 것은 아니다. 하지만 비슷한 생각을 해냈고, 시간초과가 날것이라고 생각해 검증해보지 않았다.
 너무 바보같은 짓이었다. 앞으로는 괜찮은 것 같은 방법이 떠오르면 일단 한번 해보고 어떤결과가 나오는지 검증을 꼭 해봐야겠다.
+*/
 */
