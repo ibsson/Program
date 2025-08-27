2b91ac17a386d7c6fb3bd7014f10f5a4c3088691
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-27 15:30:44 +0900

    Update and rename 20240815_bkjn_11724(연결 요소의 개수).cppD to 20240815_bkjn_11724(연결 요소의 개수).cpp

--- BKJN/그래프/DFS/20240815_bkjn_11724(연결 요소의 개수).cpp
+++ BKJN/그래프/DFS/20240815_bkjn_11724(연결 요소의 개수).cpp
@@ -1,3 +1,50 @@
+#include <iostream>
+#include <vector>
+#define MAX 1001
+using namespace std;
+
+vector <int> graph[MAX];
+bool visited[MAX];
+
+void DFS(int cur) {
+	visited[cur] = true;
+
+	for (int i = 0; i < graph[cur].size(); i++) {
+		if (!visited[graph[cur][i]]) {
+			DFS(graph[cur][i]);
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 0; i < M; i++) {
+		int u, v;
+		cin >> u >> v;
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+
+	int cnt = 0;
+	for (int i = 1; i <= N; i++) {
+		if (!visited[i]) {
+			cnt++;
+			DFS(i);
+		}
+	}
+
+	cout << cnt << '\n';
+
+	return 0;
+}
+
+/*
 #include <stdio.h>
 
 int Vertex[1002][1002];
@@ -52,4 +99,5 @@ int main(void) {
 인접행렬과 DFS를 이용해서 풀려고 했으나 배열 크기의 1을 작게 설정해서 조금 시간이 걸렸다.
 이 문제를 통해서 알게 된 것은 걸리는 시간은 인접리스트보다 인접행렬이 더 빠르다는 사실을 알게 되었다.
 그리고 항상 느끼는 것이지만 배열크기 설정을 잘 해야겠다..
+*/
 */
