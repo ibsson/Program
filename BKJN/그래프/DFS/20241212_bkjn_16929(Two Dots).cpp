13571cf9fbbd9108a1cda0a948ec996532762535
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-29 11:25:14 +0900

    Update 20241212_bkjn_16929(Two Dots).cpp

--- BKJN/그래프/DFS/20241212_bkjn_16929(Two Dots).cpp
+++ BKJN/그래프/DFS/20241212_bkjn_16929(Two Dots).cpp
@@ -1,4 +1,63 @@
 #include <iostream>
+#define MAX 51
+using namespace std;
+
+string str[MAX];
+bool visited[MAX][MAX];
+int dy[4] = { -1, 0, 1, 0 };
+int dx[4] = { 0, -1, 0, 1 };
+int N, M;
+bool isCycle = false;
+
+void DFS(int y, int x, int py, int px, char dot) {
+    if (isCycle) return;
+
+    visited[y][x] = true;
+
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i], nx = x + dx[i];
+        if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+        if (str[ny][nx] != dot) continue;
+
+        if (!visited[ny][nx]) {
+            DFS(ny, nx, y, x, dot);
+        }
+        else if (!(ny == py && nx == px)) {
+            isCycle = true;
+            return;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        cin >> str[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (!visited[i][j]) {
+                DFS(i, j, -1, -1, str[i][j]);
+                if (isCycle) {
+                    cout << "Yes\n";
+                    return 0;
+                }
+            }
+        }
+    }
+
+    cout << "No\n";
+
+    return 0;
+}
+
+/*
+#include <iostream>
 #include <string.h>
 using namespace std;
 
@@ -69,4 +128,5 @@ DFS를 사용해 인접한 점이 같은 색깔이면 그 점으로 이동하고
 생각한대로 잘 풀려서 좋았다.
 더 깔끔하고 좋은 풀이가 있을 것 같다.
 한번 찾아보고 공부해봐야겠다.
+*/
 */
