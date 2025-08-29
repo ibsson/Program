eaca8be39fa21c312357faa8242085f62e45d5de
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-08-29 14:58:39 +0900

    Update 20241227_bkjn_16940(BFS 스페셜 저지)(Sort STL 사용자 지정 Compare 함수 활용).cpp

--- BKJN/그래프/BFS/20241227_bkjn_16940(BFS 스페셜 저지)(Sort STL 사용자 지정 Compare 함수 활용).cpp
+++ BKJN/그래프/BFS/20241227_bkjn_16940(BFS 스페셜 저지)(Sort STL 사용자 지정 Compare 함수 활용).cpp
@@ -2,6 +2,80 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#define MAX 100002
+using namespace std;
+
+vector <int> graph[MAX];
+int num[MAX];
+bool visited[MAX];
+int cnt = 1;
+
+bool BFS(int x) {
+	queue <int> q;
+
+	visited[x] = true;
+	q.push(x);
+
+	while (!q.empty()) {
+		int cur = q.front();
+		q.pop();
+
+		if (cnt != num[cur]) return false;
+
+		cnt++;
+		for (int i = 0; i < graph[cur].size(); i++) {
+			int next = graph[cur][i];
+
+			if (!visited[next]) {
+				visited[next] = true;
+				q.push(next);
+			}
+		}
+	}
+
+	return true;
+}
+
+bool Compare(int& a, int& b) {
+	return num[a] < num[b];
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N = 0;
+	cin >> N;
+
+	for (int i = 0; i < (N - 1); i++) {
+		int u, v;
+		cin >> u >> v;
+
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+
+	for (int i = 1; i <= N; i++) {
+		int x = 0;
+		cin >> x;
+		num[x] = i;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		sort(graph[i].begin(), graph[i].end(), [](int a, int b) {return num[a] < num[b]; });
+	}
+
+	cout << BFS(1) << "\n";
+
+	return 0;
+}
+
+/*
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
 #define MAX 100002
@@ -81,4 +155,5 @@ int main(void) {
 인접리스트에서 노드들의 순서대로 방문한다는 것을 이용해서 내가 검사하려는 순서를 기준으로 각 노드들을 정렬하고 풀면 되는 문제였다.
 정말 어려운 문제였지만 얻어가는것도 굉장히 많은 문제였던 것 같다.
 이전까지는 오름차순, 내림차순 으로밖에 정렬하지 못했지만 앞으로 정렬을 할때 정렬 기준을 내가 원하는대로 설정 할 수 있을 것 같다.
+*/
 */
