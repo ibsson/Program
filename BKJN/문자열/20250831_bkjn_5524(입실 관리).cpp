5d4a4b29e6bd499e37596fcd10c0bdbacf487b4a
Beomsoo Son <46486181+ibsson@users.noreply.github.com>
2025-09-01 00:49:44 +0900

    20250831_bkjn_5524(입실 관리).cpp 만들기

---
+++ BKJN/문자열/20250831_bkjn_5524(입실 관리).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int n; string input;
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> input;
+		for (int i = 0; i < input.length(); i++) {
+			input[i] = tolower(input[i]);
+		}
+		cout << input << endl;
+	}
+}
